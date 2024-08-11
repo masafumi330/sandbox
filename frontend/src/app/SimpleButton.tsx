ab92bf24f06499c6b591c950950b916eab48ee90
masafumi330 <mihusamawasami@gmail.com>
2024-08-11 17:51:58 +0900

    jest test

---
+++ frontend/src/app/SimpleButton.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { useState } from "react";
+
+export const SimpleButton: () => JSX.Element = () => {
+  const [state, setState] = useState(false);
+  const handleClick = () => {
+    setState((prevState) => !prevState);
+  };
+  return <button onClick={handleClick}>{state ? "ON" : "OFF"}</button>;
+};
