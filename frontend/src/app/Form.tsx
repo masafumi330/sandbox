9ff028fe2eb5c4d4533a424781f48a660e7d08a8
masafumi330 <mihusamawasami@gmail.com>
2024-08-19 23:31:10 +0900

    :zap: MUI導入

--- frontend/src/app/Form.tsx
+++ frontend/src/app/Form.tsx
@@ -1,12 +1,11 @@
-'use client'
-import { useForm, SubmitHandler } from "react-hook-form"
-
+"use client";
+import { useForm, SubmitHandler } from "react-hook-form";
+import Button from "@mui/material/Button";
 
 type Inputs = {
-  example: string
-  exampleRequired: string
-}
-
+  example: string;
+  exampleRequired: string;
+};
 
 export default function App() {
   const {
@@ -14,27 +13,28 @@ export default function App() {
     handleSubmit,
     watch,
     formState: { errors },
-  } = useForm<Inputs>()
-  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data.exampleRequired)
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) =>
+    console.log(data.exampleRequired);
 
-
-  console.log(watch("example")) // watch input value by passing the name of it
-
+  console.log(watch("example")); // watch input value by passing the name of it
 
   return (
-    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
-    <form onSubmit={handleSubmit(onSubmit)}>
-      {/* register your input into the hook by invoking the "register" function */}
-      <input defaultValue="test" {...register("example")} />
+    <>
+      <Button variant="contained">Hello World</Button>
 
+      {/* "handleSubmit" will validate your inputs before invoking "onSubmit" */}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {/* register your input into the hook by invoking the "register" function */}
+        <input defaultValue="test" {...register("example")} />
 
-      {/* include validation with required or other standard HTML validation rules */}
-      <input {...register("exampleRequired", { required: true })} />
-      {/* errors will return when field validation fails  */}
-      {errors.exampleRequired && <span>This field is required</span>}
+        {/* include validation with required or other standard HTML validation rules */}
+        <input {...register("exampleRequired", { required: true })} />
+        {/* errors will return when field validation fails  */}
+        {errors.exampleRequired && <span>This field is required</span>}
 
-
-      <input type="submit" />
-    </form>
-  )
+        <input type="submit" />
+      </form>
+    </>
+  );
 }
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,8 +2,6 @@ import App from "./Form";
 
 export default function Home() {
   return (
-    <div>
-      <App />
-    </div>
+    <App />
   );
 }
